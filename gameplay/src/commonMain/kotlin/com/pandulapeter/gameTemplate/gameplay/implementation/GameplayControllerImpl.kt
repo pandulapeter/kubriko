44ad8591b79802583fcdb382a5c5941c60b0551f
Pandula Péter <pandulapeter@gmail.com>
2024-10-25 15:32:22 +0300

    Implement scaling.

--- gameplay/src/commonMain/kotlin/com/pandulapeter/gameTemplate/gameplay/implementation/GameplayControllerImpl.kt
+++ gameplay/src/commonMain/kotlin/com/pandulapeter/gameTemplate/gameplay/implementation/GameplayControllerImpl.kt
@@ -46,32 +46,27 @@ internal object GameplayControllerImpl : GameplayController, CoroutineScope {
 
     override fun start() {
         Engine.get().gameObjectManager.register(
-            (0..360).let { angleRange ->
-                (50..100).let { sizeRange ->
-                    (-80..80).let { offsetRange ->
-                        (-RECTANGLE_COUNT..RECTANGLE_COUNT).flatMap { x ->
-                            (-RECTANGLE_COUNT..RECTANGLE_COUNT).map { y ->
-                                StaticBox(
-                                    color = Color.hsv(angleRange.random().toFloat(), 0.2f, 0.9f),
-                                    edgeSize = RECTANGLE_SIZE * (sizeRange.random() / 100f),
-                                    position = Offset(x * RECTANGLE_DISTANCE + offsetRange.random(), y * RECTANGLE_DISTANCE + offsetRange.random()),
-                                    rotationDegrees = angleRange.random().toFloat(),
-                                )
-                            }
-                        }
-                    }
-                }
-            } + (0..360).let { angleRange ->
-                (50..100).let { sizeRange ->
-                    (-80..80).let { offsetRange ->
-                        (-RECTANGLE_COUNT..RECTANGLE_COUNT).flatMap { x ->
-                            (-RECTANGLE_COUNT..RECTANGLE_COUNT).map { y ->
-                                DynamicBox(
-                                    color = Color.hsv(angleRange.random().toFloat(), 0.2f, 0.9f),
-                                    edgeSize = RECTANGLE_SIZE * (sizeRange.random() / 100f),
-                                    position = Offset(x * RECTANGLE_DISTANCE + offsetRange.random(), y * RECTANGLE_DISTANCE + offsetRange.random()),
-                                    rotationDegrees = angleRange.random().toFloat(),
-                                )
+            listOf(true, false).let { booleanRange ->
+                (0..360).let { angleRange ->
+                    (50..100).let { sizeRange ->
+                        (50..150).let { scaleRange ->
+                            (-80..80).let { offsetRange ->
+                                (-RECTANGLE_COUNT..RECTANGLE_COUNT).flatMap { x ->
+                                    (-RECTANGLE_COUNT..RECTANGLE_COUNT).map { y ->
+                                        if (booleanRange.random()) StaticBox(
+                                            color = Color.hsv(angleRange.random().toFloat(), 0.2f, 0.9f),
+                                            edgeSize = RECTANGLE_SIZE * (sizeRange.random() / 100f),
+                                            position = Offset(x * RECTANGLE_DISTANCE + offsetRange.random(), y * RECTANGLE_DISTANCE + offsetRange.random()),
+                                            rotationDegrees = angleRange.random().toFloat(),
+                                        ) else DynamicBox(
+                                            color = Color.hsv(angleRange.random().toFloat(), 0.2f, 0.9f),
+                                            edgeSize = RECTANGLE_SIZE * (sizeRange.random() / 100f),
+                                            position = Offset(x * RECTANGLE_DISTANCE + offsetRange.random(), y * RECTANGLE_DISTANCE + offsetRange.random()),
+                                            rotationDegrees = angleRange.random().toFloat(),
+                                            scaleFactor = scaleRange.random() / 100f,
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
--- gameplay/src/commonMain/kotlin/com/pandulapeter/gameTemplate/gameplay/implementation/gameObjects/DynamicBox.kt
+++ gameplay/src/commonMain/kotlin/com/pandulapeter/gameTemplate/gameplay/implementation/gameObjects/DynamicBox.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.graphics.drawscope.DrawScope
 import com.pandulapeter.gameTemplate.engine.gameObject.properties.Dynamic
 import com.pandulapeter.gameTemplate.engine.gameObject.GameObject
 import com.pandulapeter.gameTemplate.engine.gameObject.properties.Rotatable
+import com.pandulapeter.gameTemplate.engine.gameObject.properties.Scalable
 import com.pandulapeter.gameTemplate.engine.gameObject.properties.Visible
 
 internal data class DynamicBox(
@@ -14,14 +15,27 @@ internal data class DynamicBox(
     val edgeSize: Float,
     override var position: Offset,
     override var rotationDegrees: Float,
-) : GameObject(), Visible, Dynamic, Rotatable {
+    override var scaleFactor: Float
+) : GameObject(), Visible, Dynamic, Rotatable, Scalable {
 
     override val size = Size(edgeSize, edgeSize)
+    private var isGrowing = true
 
     override fun update(deltaTimeMillis: Float) {
         rotationDegrees += 0.1f * deltaTimeMillis
         while (rotationDegrees > 360f) {
             rotationDegrees -= 360f
+        }
+        if (scaleFactor >= 1.6f) {
+            isGrowing = false
+        }
+        if (scaleFactor <= 0.5f) {
+            isGrowing = true
+        }
+        if (isGrowing) {
+            scaleFactor += 0.001f * deltaTimeMillis
+        } else {
+            scaleFactor -= 0.001f * deltaTimeMillis
         }
     }
 
