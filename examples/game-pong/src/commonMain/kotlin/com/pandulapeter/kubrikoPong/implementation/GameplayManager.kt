f565cb9c9e1d74e58dfc1d60bbb3b1b354f9e9c8
Pandula Péter <pandulapeter@gmail.com>
2024-11-06 21:37:16 +0200

    Implement Pong ball movement.

--- examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/implementation/GameplayManager.kt
+++ examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/implementation/GameplayManager.kt
@@ -5,6 +5,7 @@ import com.pandulapeter.kubriko.implementation.extensions.require
 import com.pandulapeter.kubriko.manager.ActorManager
 import com.pandulapeter.kubriko.manager.Manager
 import com.pandulapeter.kubriko.manager.StateManager
+import com.pandulapeter.kubriko.manager.ViewportManager
 import com.pandulapeter.kubrikoPong.implementation.actors.Ball
 import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.launchIn
@@ -13,17 +14,15 @@ import kotlinx.coroutines.flow.onEach
 // TODO: Implement Pong game logic
 internal class GameplayManager : Manager() {
 
-    private lateinit var actorManager: ActorManager
     lateinit var stateManager: StateManager
         private set
 
     override fun onInitialize(kubriko: Kubriko) {
-        actorManager = kubriko.require()
         stateManager = kubriko.require()
         stateManager.isFocused
             .filterNot { it }
             .onEach { stateManager.updateIsRunning(false) }
             .launchIn(scope)
-        actorManager.add(Ball())
+        kubriko.require<ActorManager>().add(Ball(kubriko.require<ViewportManager>()))
     }
 }
--- examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/implementation/actors/Ball.kt
+++ examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/implementation/actors/Ball.kt
@@ -1,17 +1,63 @@
 package com.pandulapeter.kubrikoPong.implementation.actors
 
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import com.pandulapeter.kubriko.implementation.extensions.scenePixel
+import com.pandulapeter.kubriko.actor.traits.Dynamic
 import com.pandulapeter.kubriko.actor.traits.Visible
+import com.pandulapeter.kubriko.implementation.extensions.scenePixel
+import com.pandulapeter.kubriko.implementation.extensions.toSceneOffset
+import com.pandulapeter.kubriko.manager.ViewportManager
 import com.pandulapeter.kubriko.types.SceneOffset
 import com.pandulapeter.kubriko.types.SceneSize
 
-class Ball(
-    override val boundingBox: SceneSize = SceneSize(100f.scenePixel, 100f.scenePixel),
-    override var position: SceneOffset = SceneOffset.Zero,
-    private val boxColor: Color = Color.Gray,
-) : Visible {
+class Ball(private val viewportManager: ViewportManager) : Visible, Dynamic {
+
+    private val size = 20f.scenePixel
+    private val speed = 1.5f.scenePixel
+    override val boundingBox: SceneSize = SceneSize(size, size)
+    override var position: SceneOffset = SceneOffset.Zero
+    private val boxColor: Color = Color.Gray
+    private var speedX = speed
+    private var speedY = speed
+
+    override fun update(deltaTimeInMillis: Float) {
+        val viewportCenter = viewportManager.cameraPosition.value
+        val viewportSize = viewportManager.size.value
+        val viewportScaleFactor = viewportManager.scaleFactor.value
+        val viewportTopLeft = Offset.Zero.toSceneOffset(
+            viewportCenter = viewportCenter,
+            viewportSize = viewportSize,
+            viewportScaleFactor = viewportScaleFactor,
+        )
+        val viewportBottomRight = Offset(viewportSize.width, viewportSize.height).toSceneOffset(
+            viewportCenter = viewportCenter,
+            viewportSize = viewportSize,
+            viewportScaleFactor = viewportScaleFactor,
+        )
+        // Validate current position to handle potential bugs caused by resizing the screen
+        if (position.x < viewportTopLeft.x) {
+            position = SceneOffset(viewportTopLeft.x + size, position.y)
+        }
+        if (position.x > viewportBottomRight.x) {
+            position = SceneOffset(viewportBottomRight.x - size, position.y)
+        }
+        if (position.y < viewportTopLeft.y) {
+            position = SceneOffset(position.x, viewportTopLeft.y + size)
+        }
+        if (position.y > viewportBottomRight.y) {
+            position = SceneOffset(position.x, viewportBottomRight.y - size)
+        }
+        // Validate the next position, bounce if needed
+        val nextPosition = position + SceneOffset(speedX, speedY) * deltaTimeInMillis
+        if (nextPosition.x < viewportTopLeft.x || nextPosition.x > viewportBottomRight.x) {
+            speedX *= -1
+        }
+        if (nextPosition.y < viewportTopLeft.y || nextPosition.y > viewportBottomRight.y) {
+            speedY *= -1
+        }
+        position = nextPosition
+    }
 
     override fun draw(scope: DrawScope) = scope.drawCircle(
         color = boxColor,
