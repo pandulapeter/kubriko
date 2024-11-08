611ccaa854dc4896fc9d6406bc918e21d9331e73
Pandula Péter <pandulapeter@gmail.com>
2024-11-08 10:44:49 +0200

    Improve the appearance of physics bouncy balls.

--- examples/test-physics/src/commonMain/kotlin/com/pandulapeter/kubrikoPhysicsTest/implementation/actors/BouncyBallActor.kt
+++ examples/test-physics/src/commonMain/kotlin/com/pandulapeter/kubrikoPhysicsTest/implementation/actors/BouncyBallActor.kt
@@ -1,17 +1,20 @@
 package com.pandulapeter.kubrikoPhysicsTest.implementation.actors
 
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import com.pandulapeter.kubriko.actor.traits.Dynamic
 import com.pandulapeter.kubriko.actor.traits.Visible
+import com.pandulapeter.kubriko.implementation.extensions.rad
 import com.pandulapeter.kubriko.implementation.extensions.scenePixel
 import com.pandulapeter.kubriko.physicsManager.implementation.dynamics.Body
+import com.pandulapeter.kubriko.types.AngleRadians
 import com.pandulapeter.kubriko.types.SceneOffset
 import com.pandulapeter.kubriko.types.SceneSize
 
 class BouncyBallActor(
     private val body: Body,
-    radius: Float,
+    private val radius: Float,
 ) : Visible, Dynamic {
 
     override val boundingBox: SceneSize = SceneSize(radius.scenePixel, radius.scenePixel)
@@ -21,6 +24,7 @@ class BouncyBallActor(
             it.y.toFloat().scenePixel,
         )
     }
+    override var rotation: AngleRadians = AngleRadians.Zero
 
     override fun update(deltaTimeInMillis: Float) {
         position = body.position.let {
@@ -29,11 +33,26 @@ class BouncyBallActor(
                 it.y.toFloat().scenePixel,
             )
         }
+        rotation = body.orientation.toFloat().rad
     }
 
-    override fun draw(scope: DrawScope) = scope.drawCircle(
-        color = Color.White,
-        radius = (boundingBox.raw.width + boundingBox.raw.height) / 2,
-        center = pivotOffset.raw,
-    )
+    override fun draw(scope: DrawScope) {
+        scope.drawCircle(
+            color = Color.White,
+            radius = radius,
+            center = pivotOffset.raw,
+        )
+        scope.drawLine(
+            color = Color.Black,
+            start = Offset(pivotOffset.raw.x - radius, pivotOffset.raw.y),
+            end = Offset(pivotOffset.raw.x + radius, pivotOffset.raw.y),
+            strokeWidth = 2f,
+        )
+        scope.drawLine(
+            color = Color.Black,
+            start = Offset(pivotOffset.raw.x, pivotOffset.raw.y - radius),
+            end = Offset(pivotOffset.raw.x, pivotOffset.raw.y + radius),
+            strokeWidth = 2f,
+        )
+    }
 }
