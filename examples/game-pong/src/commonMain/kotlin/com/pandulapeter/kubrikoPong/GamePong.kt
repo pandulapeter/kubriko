9a34c17f7123fc0368cd52b19caeb508e65ea47e
Pandula Péter <pandulapeter@gmail.com>
2024-11-08 22:44:02 +0200

    Implement the CloudShader.

--- examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/GamePong.kt
+++ examples/game-pong/src/commonMain/kotlin/com/pandulapeter/kubrikoPong/GamePong.kt
@@ -7,7 +7,6 @@ import com.pandulapeter.kubriko.Kubriko
 import com.pandulapeter.kubriko.KubrikoCanvas
 import com.pandulapeter.kubriko.shader.ShaderManager
 import com.pandulapeter.kubriko.shader.collection.RippleShader
-import com.pandulapeter.kubrikoPong.implementation.BackgroundManager
 import com.pandulapeter.kubrikoPong.implementation.GameplayManager
 
 @Composable
@@ -16,7 +15,6 @@ fun GamePong(
 ) {
     val kubriko = remember {
         Kubriko.newInstance(
-            BackgroundManager(),
             ShaderManager.newInstance(RippleShader(canvasIndex = -1)),
             GameplayManager(),
         )
