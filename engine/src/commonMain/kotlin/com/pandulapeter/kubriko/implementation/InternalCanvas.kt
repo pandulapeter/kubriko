ebc7a1c6c0b014d01c71d487a013ff7ee60a93fe
Pandula Péter <pandulapeter@gmail.com>
2024-11-21 10:11:39 +0200

    Clip Visible Actor drawing scope to its bounding box.

--- engine/src/commonMain/kotlin/com/pandulapeter/kubriko/implementation/InternalCanvas.kt
+++ engine/src/commonMain/kotlin/com/pandulapeter/kubriko/implementation/InternalCanvas.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.drawscope.withTransform
 import com.pandulapeter.kubriko.actor.traits.Overlay
 import com.pandulapeter.kubriko.actor.traits.Visible
@@ -44,7 +45,14 @@ internal fun InternalCanvas(
                             .forEach { visible ->
                                 withTransform(
                                     transformBlock = { visible.transform(this) },
-                                    drawBlock = { visible.draw(this) }
+                                    drawBlock = {
+                                        clipRect(
+                                            right = visible.boundingBox.width.raw,
+                                            bottom = visible.boundingBox.height.raw,
+                                        ) {
+                                            visible.draw(this)
+                                        }
+                                    }
                                 )
                             }
                     }
