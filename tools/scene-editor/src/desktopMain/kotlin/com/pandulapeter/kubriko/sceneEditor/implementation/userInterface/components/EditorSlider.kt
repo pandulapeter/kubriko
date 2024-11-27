a56e2701969c4bbde761116723e2d1b23c4cc09e
Pandula Péter <pandulapeter@gmail.com>
2024-11-27 21:22:37 +0200

    Implement the infinite slider.

--- tools/scene-editor/src/desktopMain/kotlin/com/pandulapeter/kubriko/sceneEditor/implementation/userInterface/components/EditorSlider.kt
+++ tools/scene-editor/src/desktopMain/kotlin/com/pandulapeter/kubriko/sceneEditor/implementation/userInterface/components/EditorSlider.kt
@@ -13,9 +13,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlin.math.abs
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,20 +33,35 @@ internal fun EditorSlider(
 ) = Column(
     modifier = modifier,
 ) {
+    val add = remember { mutableStateOf(0f) }
+    val interactionSource = remember { MutableInteractionSource() }
     if (title.isNotBlank()) {
         EditorTextLabel(
             text = "$title: ${"%.2f".format(value)}$suffix",
         )
     }
-    val interactionSource = remember { MutableInteractionSource() }
     val colors = SliderDefaults.colors().copy(
         inactiveTrackColor = MaterialTheme.colorScheme.primary,
     )
+    SideEffect {
+        if (valueRange == null && add.value != 0f) {
+            onValueChanged(value + add.value.toDifference())
+        }
+    }
     Slider(
         modifier = Modifier.height(16.dp),
-        value = value,
-        onValueChange = onValueChanged,
-        valueRange = valueRange ?: -100f..100f, // TODO: Intelligent slider
+        value = if (valueRange == null) add.value else value,
+        onValueChange = {
+            if (valueRange == null) {
+                add.value = it
+            } else {
+                onValueChanged(it)
+            }
+        },
+        onValueChangeFinished = {
+            add.value = 0f
+        },
+        valueRange = valueRange ?: -5f..5f,
         enabled = enabled,
         interactionSource = interactionSource,
         thumb = {
@@ -65,4 +83,12 @@ internal fun EditorSlider(
             )
         }
     )
+}
+
+private fun Float.toDifference() = when (abs(this)) {
+    in 0f..0.5f -> this / 1000
+    in 0.5f..1.5f -> this / 100
+    in 1.5f..3.5f -> this / 10
+    in 3.5f..4.9f -> this
+    else -> this * 2
 }
