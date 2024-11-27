032618b9ec86b7a249a57dc4c56acca712aa1188
Pandula Péter <pandulapeter@gmail.com>
2024-11-27 17:32:20 +0200

    Reduce editor text input size.

--- tools/scene-editor/src/desktopMain/kotlin/com/pandulapeter/kubriko/sceneEditor/implementation/userInterface/components/EditorTextInput.kt
+++ tools/scene-editor/src/desktopMain/kotlin/com/pandulapeter/kubriko/sceneEditor/implementation/userInterface/components/EditorTextInput.kt
@@ -1,9 +1,17 @@
 package com.pandulapeter.kubriko.sceneEditor.implementation.userInterface.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 
 @Composable
 internal fun EditorTextInput(
@@ -19,9 +27,30 @@ internal fun EditorTextInput(
         text = title,
     )
     // TODO: Focusing this fields should take focus away from the EngineCanvas to avoid navigation using the arrow keys.
-    OutlinedTextField(
+    BasicTextField(
+        modifier = modifier
+            .background(
+                MaterialTheme.colorScheme.surface,
+                MaterialTheme.shapes.small,
+            )
+            .fillMaxWidth(),
         value = value,
         enabled = enabled,
         onValueChange = onValueChanged,
+        singleLine = true,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        textStyle = LocalTextStyle.current.copy(
+            color = MaterialTheme.colorScheme.onSurface,
+        ),
+        decorationBox = { innerTextField ->
+            Row(
+                modifier,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(Modifier.weight(1f)) {
+                    innerTextField()
+                }
+            }
+        }
     )
 }
