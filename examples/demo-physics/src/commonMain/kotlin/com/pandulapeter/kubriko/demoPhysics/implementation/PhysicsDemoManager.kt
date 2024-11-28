ad0b48e149db875b452b1e040e832e5676635a3a
Pandula Péter <pandulapeter@gmail.com>
2024-11-28 16:01:29 +0200

    Create the space for the Joints demo.

--- examples/demo-physics/src/commonMain/kotlin/com/pandulapeter/kubriko/demoPhysics/implementation/PhysicsDemoManager.kt
+++ examples/demo-physics/src/commonMain/kotlin/com/pandulapeter/kubriko/demoPhysics/implementation/PhysicsDemoManager.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.kubriko.demoPhysics.implementation
 
 import com.pandulapeter.kubriko.Kubriko
+import com.pandulapeter.kubriko.actor.Actor
 import com.pandulapeter.kubriko.demoPhysics.implementation.actors.BouncyBall
 import com.pandulapeter.kubriko.demoPhysics.implementation.actors.Platform
 import com.pandulapeter.kubriko.implementation.extensions.require
@@ -11,6 +12,8 @@ import com.pandulapeter.kubriko.types.SceneOffset
 import com.pandulapeter.kubriko.types.SceneSize
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
 internal class PhysicsDemoManager : Manager() {
@@ -21,21 +24,30 @@ internal class PhysicsDemoManager : Manager() {
 
     override fun onInitialize(kubriko: Kubriko) {
         actorManager = kubriko.require()
-        actorManager.add(
-            actors = ((0..40).map {
-                BouncyBall(
-                    radius = (10..50).random().toFloat().scenePixel,
-                    initialPosition = SceneOffset(
-                        x = (-500..500).random().toFloat().scenePixel,
-                        y = (-800..0).random().toFloat().scenePixel,
-                    ),
-                )
-            } + Platform(
-                initialPosition = SceneOffset(0f.scenePixel, 350f.scenePixel),
-                boundingBox = SceneSize(800f.scenePixel, 40f.scenePixel),
-            )).toTypedArray()
-        )
+        demoType.onEach(::resetDemo).launchIn(scope)
     }
 
     fun setSelectedDemoType(demoType: PhysicsDemoType) = _demoType.update { demoType }
+
+    private fun resetDemo(demoType: PhysicsDemoType) {
+        actorManager.removeAll()
+        actorManager.add(actors = demoType.createActors())
+    }
+
+    private fun PhysicsDemoType.createActors(): Array<Actor> = when (this) {
+        PhysicsDemoType.RIGID_BODY_COLLISIONS -> ((0..40).map {
+            BouncyBall(
+                radius = (10..50).random().toFloat().scenePixel,
+                initialPosition = SceneOffset(
+                    x = (-500..500).random().toFloat().scenePixel,
+                    y = (-800..0).random().toFloat().scenePixel,
+                ),
+            )
+        } + Platform(
+            initialPosition = SceneOffset(0f.scenePixel, 350f.scenePixel),
+            boundingBox = SceneSize(800f.scenePixel, 40f.scenePixel),
+        ))
+
+        PhysicsDemoType.JOINTS -> emptyList()
+    }.toTypedArray()
 }
--- examples/demo-physics/src/commonMain/kotlin/com/pandulapeter/kubriko/demoPhysics/implementation/PhysicsDemoType.kt
+++ examples/demo-physics/src/commonMain/kotlin/com/pandulapeter/kubriko/demoPhysics/implementation/PhysicsDemoType.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.kubriko.demoPhysics.implementation
 
 import kubriko.examples.demo_physics.generated.resources.Res
+import kubriko.examples.demo_physics.generated.resources.joints
 import kubriko.examples.demo_physics.generated.resources.rigid_body_collisions
 import org.jetbrains.compose.resources.StringResource
 
@@ -8,4 +9,5 @@ internal enum class PhysicsDemoType(
     val nameStringResource: StringResource,
 ) {
     RIGID_BODY_COLLISIONS(nameStringResource = Res.string.rigid_body_collisions),
+    JOINTS(nameStringResource = Res.string.joints),
 }
