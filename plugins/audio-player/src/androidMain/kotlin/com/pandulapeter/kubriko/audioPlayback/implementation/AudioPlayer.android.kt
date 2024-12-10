c8c383645e15e34f006a7ee261d0fceb0d155214
Pandula Péter <pandulapeter@gmail.com>
2024-12-10 19:53:58 +0200

    Implement the Android audio player.

--- plugins/audio-player/src/androidMain/kotlin/com/pandulapeter/kubriko/audioPlayback/implementation/AudioPlayer.android.kt
+++ plugins/audio-player/src/androidMain/kotlin/com/pandulapeter/kubriko/audioPlayback/implementation/AudioPlayer.android.kt
@@ -1,13 +1,21 @@
 package com.pandulapeter.kubriko.audioPlayback.implementation
 
+import android.media.MediaPlayer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 
 @Composable
 internal actual fun rememberAudioPlayer() = remember {
     object : AudioPlayer {
-        // TODO: Android audio player
-        override fun play(uri: String) = println("[Android] play $uri")
+
+        override fun play(uri: String) {
+            MediaPlayer().run {
+                setDataSource(uri)
+                setOnPreparedListener { it.start() }
+                setOnCompletionListener { it.release() }
+                prepareAsync()
+            }
+        }
 
         override fun dispose() = Unit
     }
