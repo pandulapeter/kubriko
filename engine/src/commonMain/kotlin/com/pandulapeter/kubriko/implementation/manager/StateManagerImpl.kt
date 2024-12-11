86312cc93b2ff3562c91d0a27586eb6c79489554
Pandula Péter <pandulapeter@gmail.com>
2024-12-11 19:40:49 +0200

    Debounce focus changes.

--- engine/src/commonMain/kotlin/com/pandulapeter/kubriko/implementation/manager/StateManagerImpl.kt
+++ engine/src/commonMain/kotlin/com/pandulapeter/kubriko/implementation/manager/StateManagerImpl.kt
@@ -1,17 +1,19 @@
 package com.pandulapeter.kubriko.implementation.manager
 
 import com.pandulapeter.kubriko.manager.StateManager
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 
+@OptIn(FlowPreview::class)
 internal class StateManagerImpl(
     val shouldAutoStart: Boolean,
 ) : StateManager() {
 
     private val _isFocused = MutableStateFlow(true)
-    override val isFocused by autoInitializingLazy { _isFocused.asStateFlow() }
+    override val isFocused by autoInitializingLazy { _isFocused.debounce(300).asStateFlow(true) }
     private val _isRunning = MutableStateFlow(false)
     override val isRunning by autoInitializingLazy {
         combine(isFocused, _isRunning) { isFocused, isRunning -> isFocused && isRunning }.asStateFlow(false)
