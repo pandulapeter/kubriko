b33dfdc336f8b569708a5a1e249d17069896be18
Pandula Péter <pandulapeter@gmail.com>
2024-12-12 12:27:36 +0200

    Implement the KeyValuePersistenceManager for Android.

--- plugins/persistence/src/androidMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.android.kt
+++ plugins/persistence/src/androidMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.android.kt
@@ -1,5 +1,21 @@
 package com.pandulapeter.kubriko.persistence.implementation
 
-internal actual fun createKeyValuePersistenceManager(fileName: String): KeyValuePersistenceManager {
-    TODO("Not yet implemented")
+import android.content.Context
+import com.pandulapeter.kubriko.ActivityHolder
+
+internal actual fun createKeyValuePersistenceManager(fileName: String) = object : KeyValuePersistenceManager {
+
+    private val preferences by lazy { ActivityHolder.currentActivity.value!!.applicationContext.getSharedPreferences(fileName, Context.MODE_PRIVATE) }
+
+    override fun getBoolean(key: String, defaultValue: Boolean) = preferences.getBoolean(key, defaultValue)
+
+    override fun putBoolean(key: String, value: Boolean) = preferences.edit().putBoolean(key, value).apply()
+
+    override fun getInt(key: String, defaultValue: Int) = preferences.getInt(key, defaultValue)
+
+    override fun putInt(key: String, value: Int) = preferences.edit().putInt(key, value).apply()
+
+    override fun getString(key: String, defaultValue: String) = preferences.getString(key, defaultValue) ?: defaultValue
+
+    override fun putString(key: String, value: String) = preferences.edit().putString(key, value).apply()
 }
