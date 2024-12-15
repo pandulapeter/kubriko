71aad822195e13fedc0ed39fcffb63003a3ab3f1
Pandula Péter <pandulapeter@gmail.com>
2024-12-15 16:03:38 +0200

    Add an alternative background shader.

--- examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/implementation/managers/SpaceSquadronBackgroundManager.kt
+++ examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/implementation/managers/SpaceSquadronBackgroundManager.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers
 
 import com.pandulapeter.kubriko.Kubriko
+import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.actors.SimplifiedSpaceBackgroundShader
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.actors.SpaceBackgroundShader
 import com.pandulapeter.kubriko.implementation.extensions.get
 import com.pandulapeter.kubriko.manager.ActorManager
@@ -8,5 +9,7 @@ import com.pandulapeter.kubriko.manager.Manager
 
 internal class SpaceSquadronBackgroundManager : Manager() {
 
-    override fun onInitialize(kubriko: Kubriko) = kubriko.get<ActorManager>().add(SpaceBackgroundShader())
+    override fun onInitialize(kubriko: Kubriko) = kubriko.get<ActorManager>().add(
+        listOf( SpaceBackgroundShader(), SimplifiedSpaceBackgroundShader()).random()
+    )
 }
