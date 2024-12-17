ce0f43055c2a957f31d1440aa6aa218a52fa1155
Pandula Péter <pandulapeter@gmail.com>
2024-12-17 16:47:04 +0200

    Add smooth pixelation shader to the fog background.

--- examples/game-wallbreaker/src/commonMain/kotlin/com/pandulapeter/kubriko/gameWallbreaker/implementation/managers/WallbreakerBackgroundManager.kt
+++ examples/game-wallbreaker/src/commonMain/kotlin/com/pandulapeter/kubriko/gameWallbreaker/implementation/managers/WallbreakerBackgroundManager.kt
@@ -5,8 +5,9 @@ import com.pandulapeter.kubriko.gameWallbreaker.implementation.actors.FogShader
 import com.pandulapeter.kubriko.implementation.extensions.get
 import com.pandulapeter.kubriko.manager.ActorManager
 import com.pandulapeter.kubriko.manager.Manager
+import com.pandulapeter.kubriko.shader.collection.SmoothPixelationShader
 
 internal class WallbreakerBackgroundManager : Manager() {
 
-    override fun onInitialize(kubriko: Kubriko) = kubriko.get<ActorManager>().add(FogShader())
+    override fun onInitialize(kubriko: Kubriko) = kubriko.get<ActorManager>().add(FogShader(layerIndex = 0), SmoothPixelationShader())
 }
