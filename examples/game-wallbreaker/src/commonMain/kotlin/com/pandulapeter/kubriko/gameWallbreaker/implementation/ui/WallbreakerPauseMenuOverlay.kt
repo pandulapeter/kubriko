916a99c0862843d1411dce708ef8a8ebbb670739
Pandula Péter <pandulapeter@gmail.com>
2024-12-18 09:19:07 +0200

    Animate the Wallbreaker menu CTA button.

--- examples/game-wallbreaker/src/commonMain/kotlin/com/pandulapeter/kubriko/gameWallbreaker/implementation/ui/WallbreakerPauseMenuOverlay.kt
+++ examples/game-wallbreaker/src/commonMain/kotlin/com/pandulapeter/kubriko/gameWallbreaker/implementation/ui/WallbreakerPauseMenuOverlay.kt
@@ -1,6 +1,12 @@
 package com.pandulapeter.kubriko.gameWallbreaker.implementation.ui
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
@@ -15,9 +21,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.pandulapeter.kubriko.shared.ui.LargeButton
@@ -112,6 +120,15 @@ private fun TitleScreen(
     exit = scaleOut() + fadeOut(),
 ) {
     val rememberedShouldShowResumeButton = remember { shouldShowResumeButton }
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 0.9f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 800, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -125,6 +142,7 @@ private fun TitleScreen(
                 contentDescription = null,
             )
             LargeButton(
+                modifier = Modifier.scale(scale),
                 onButtonPressed = if (rememberedShouldShowResumeButton) onResumeButtonPressed else onRestartButtonPressed,
                 icon = Res.drawable.ic_play,
                 title = if (rememberedShouldShowResumeButton) Res.string.resume else Res.string.new_game,
