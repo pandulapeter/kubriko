b52ba4ea4b5ea2e8aaa5a76493f474dbcc2ef556
Pandula Péter <pandulapeter@gmail.com>
2024-12-19 07:48:37 +0200

    Implement immersive mode on Android.

--- app/src/androidMain/kotlin/com/pandulapeter/kubrikoShowcase/KubrikoShowcaseActivity.kt
+++ app/src/androidMain/kotlin/com/pandulapeter/kubrikoShowcase/KubrikoShowcaseActivity.kt
@@ -4,21 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.collectAsState
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
 class KubrikoShowcaseActivity : ComponentActivity() {
 
-    // TODO: Hide status bar and navigation bar in fullscreen mode
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        isInFullscreenMode.onEach { isInFullscreenMode ->
+            if (isInFullscreenMode) {
+                windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+                windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            } else {
+                windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+                windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+            }
+        }.launchIn(lifecycleScope)
         setContent {
             KubrikoShowcase(
-                isInFullscreenMode = isInFullscreenMode.value,
-                onFullscreenModeToggled = { isInFullscreenMode.value = !isInFullscreenMode.value },
+                isInFullscreenMode = isInFullscreenMode.collectAsState().value,
+                onFullscreenModeToggled = { isInFullscreenMode.update { currentValue -> !currentValue } },
             )
         }
     }
 }
 
-private val isInFullscreenMode = mutableStateOf(false)
+private val isInFullscreenMode = MutableStateFlow(false)
