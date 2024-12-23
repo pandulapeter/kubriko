12027422f28aaf4807f2be35853f6ed861cea3d3
Pandula Péter <pandulapeter@gmail.com>
2024-12-23 17:04:53 +0200

    Fix iOS PreferenceManager disregarding the file name.

--- plugins/persistence/src/iosMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.ios.kt
+++ plugins/persistence/src/iosMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.ios.kt
@@ -8,30 +8,32 @@ internal actual fun createKeyValuePersistenceManager(fileName: String) = object 
     private val preferences by lazy { NSUserDefaults.standardUserDefaults }
 
     override fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        preferences.registerDefaults(mapOf(key to defaultValue))
-        return preferences.boolForKey(key)
+        preferences.registerDefaults(mapOf(key.prefixed() to defaultValue))
+        return preferences.boolForKey(key.prefixed())
     }
 
-    override fun putBoolean(key: String, value: Boolean) = preferences.setBool(value, key)
+    override fun putBoolean(key: String, value: Boolean) = preferences.setBool(value, key.prefixed())
 
     override fun getInt(key: String, defaultValue: Int): Int {
-        preferences.registerDefaults(mapOf(key to defaultValue))
-        return preferences.integerForKey(key).toInt()
+        preferences.registerDefaults(mapOf(key.prefixed() to defaultValue))
+        return preferences.integerForKey(key.prefixed()).toInt()
     }
 
-    override fun putInt(key: String, value: Int) = preferences.setInteger(value.toLong(), key)
+    override fun putInt(key: String, value: Int) = preferences.setInteger(value.toLong(), key.prefixed())
 
     override fun getFloat(key: String, defaultValue: Float): Float {
-        preferences.registerDefaults(mapOf(key to defaultValue))
-        return preferences.floatForKey(key)
+        preferences.registerDefaults(mapOf(key.prefixed() to defaultValue))
+        return preferences.floatForKey(key.prefixed())
     }
 
-    override fun putFloat(key: String, value: Float) = preferences.setFloat(value, key)
+    override fun putFloat(key: String, value: Float) = preferences.setFloat(value, key.prefixed())
 
     override fun getString(key: String, defaultValue: String): String {
-        preferences.registerDefaults(mapOf(key to defaultValue))
-        return preferences.stringForKey(key) ?: defaultValue
+        preferences.registerDefaults(mapOf(key.prefixed() to defaultValue))
+        return preferences.stringForKey(key.prefixed()) ?: defaultValue
     }
 
     override fun putString(key: String, value: String) = preferences.setValue(value, key)
+
+    private fun String.prefixed() = "${fileName}_$this"
 }
