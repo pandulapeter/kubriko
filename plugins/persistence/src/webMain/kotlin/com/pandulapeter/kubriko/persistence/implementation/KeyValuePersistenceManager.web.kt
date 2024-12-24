61ca7e678b2124fabd174533dde7894046bde9fb
Pandula Péter <pandulapeter@gmail.com>
2024-12-24 06:36:03 +0200

    Fix Web PreferenceManager disregarding the file name.

--- plugins/persistence/src/webMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.web.kt
+++ plugins/persistence/src/webMain/kotlin/com/pandulapeter/kubriko/persistence/implementation/KeyValuePersistenceManager.web.kt
@@ -6,23 +6,25 @@ internal actual fun createKeyValuePersistenceManager(fileName: String) = object 
 
     private val preferences by lazy { localStorage }
 
-    override fun getBoolean(key: String, defaultValue: Boolean) = when (preferences.getItem(key)) {
+    override fun getBoolean(key: String, defaultValue: Boolean) = when (preferences.getItem(key.prefixed())) {
         null, "" -> defaultValue
         "true", "TRUE", "True", "1" -> true
         else -> false
     }
 
-    override fun putBoolean(key: String, value: Boolean) = preferences.setItem(key, if (value) "true" else "false")
+    override fun putBoolean(key: String, value: Boolean) = preferences.setItem(key.prefixed(), if (value) "true" else "false")
 
-    override fun getInt(key: String, defaultValue: Int) = preferences.getItem(key)?.toIntOrNull() ?: defaultValue
+    override fun getInt(key: String, defaultValue: Int) = preferences.getItem(key.prefixed())?.toIntOrNull() ?: defaultValue
 
-    override fun putInt(key: String, value: Int) = preferences.setItem(key, value.toString())
+    override fun putInt(key: String, value: Int) = preferences.setItem(key.prefixed(), value.toString())
 
-    override fun getFloat(key: String, defaultValue: Float) = preferences.getItem(key)?.toFloatOrNull() ?: defaultValue
+    override fun getFloat(key: String, defaultValue: Float) = preferences.getItem(key.prefixed())?.toFloatOrNull() ?: defaultValue
 
-    override fun putFloat(key: String, value: Float) = preferences.setItem(key, value.toString())
+    override fun putFloat(key: String, value: Float) = preferences.setItem(key.prefixed(), value.toString())
 
-    override fun getString(key: String, defaultValue: String) = preferences.getItem(key) ?: defaultValue
+    override fun getString(key: String, defaultValue: String) = preferences.getItem(key.prefixed()) ?: defaultValue
 
-    override fun putString(key: String, value: String) = preferences.setItem(key, value)
+    override fun putString(key: String, value: String) = preferences.setItem(key.prefixed(), value)
+
+    private fun String.prefixed() = "${fileName}_$this"
 }
