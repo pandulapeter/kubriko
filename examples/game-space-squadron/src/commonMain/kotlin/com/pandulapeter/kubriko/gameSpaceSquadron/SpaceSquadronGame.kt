7b35cb4d6b71a653f1f54cb9fc0200b6b4380e41
Pandula Péter <pandulapeter@gmail.com>
2025-01-06 13:23:53 +0200

    Implement data loading for Space Squadron.

--- examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/SpaceSquadronGame.kt
+++ examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/SpaceSquadronGame.kt
@@ -6,15 +6,18 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.pandulapeter.kubriko.Kubriko
 import com.pandulapeter.kubriko.KubrikoViewport
 import com.pandulapeter.kubriko.audioPlayback.MusicManager
@@ -24,11 +27,11 @@ import com.pandulapeter.kubriko.extensions.sceneUnit
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronAudioManager
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronBackgroundManager
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronGameManager
+import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronLoadingManager
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronUIManager
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.managers.SpaceSquadronUserPreferencesManager
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.ui.SpaceSquadronMenuOverlay
 import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.ui.SpaceSquadronTheme
-import com.pandulapeter.kubriko.gameSpaceSquadron.implementation.ui.isFontLoaded
 import com.pandulapeter.kubriko.keyboardInput.KeyboardInputManager
 import com.pandulapeter.kubriko.manager.StateManager
 import com.pandulapeter.kubriko.manager.ViewportManager
@@ -36,11 +39,8 @@ import com.pandulapeter.kubriko.persistence.PersistenceManager
 import com.pandulapeter.kubriko.pointerInput.PointerInputManager
 import com.pandulapeter.kubriko.shaders.ShaderManager
 import com.pandulapeter.kubriko.shared.ExampleStateHolder
+import com.pandulapeter.kubriko.shared.ui.LoadingIndicator
 import com.pandulapeter.kubriko.sprites.SpriteManager
-import kubriko.examples.game_space_squadron.generated.resources.Res
-import kubriko.examples.game_space_squadron.generated.resources.img_logo
-import kubriko.examples.game_space_squadron.generated.resources.sprite_ship
-import org.jetbrains.compose.resources.imageResource
 
 /**
  * Music: https://freesound.org/people/Andrewkn/sounds/474864/
@@ -53,34 +53,52 @@ fun SpaceSquadronGame(
     onFullscreenModeToggled: () -> Unit = {},
 ) = SpaceSquadronTheme {
     stateHolder as SpaceSquadronGameStateHolderImpl
-    val spriteLoadingProgress = remember { stateHolder.spriteManager.getLoadingProgress(setOf(Res.drawable.sprite_ship)) }
-    val isGameLoaded = isFontLoaded() && imageResource(Res.drawable.img_logo).width > 1 && spriteLoadingProgress.collectAsState(0f).value == 1f
+    val isGameLoaded = stateHolder.loadingManager.isGameLoaded()
     KubrikoViewport(
         modifier = Modifier.fillMaxSize().background(Color.Black),
         kubriko = stateHolder.backgroundKubriko,
     )
-    KubrikoViewport(
-        kubriko = stateHolder.kubriko,
-        windowInsets = windowInsets,
-    )
+    AnimatedVisibility(
+        visible = !isGameLoaded,
+        enter = fadeIn(),
+        exit = fadeOut(),
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize().windowInsetsPadding(windowInsets).padding(16.dp),
+        ) {
+            LoadingIndicator(
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+        }
+    }
     AnimatedVisibility(
         visible = isGameLoaded,
-        enter = fadeIn() + scaleIn(),
-        exit = scaleOut() + fadeOut(),
+        enter = fadeIn() + scaleIn(initialScale = 0.88f),
+        exit = scaleOut(targetScale = 0.88f) + fadeOut(),
     ) {
-        SpaceSquadronMenuOverlay(
-            modifier = Modifier.windowInsetsPadding(windowInsets),
-            isGameRunning = stateHolder.stateManager.isRunning.collectAsState().value,
-            onPlayButtonPressed = { stateHolder.stateManager.updateIsRunning(true) },
-            onPauseButtonPressed = { stateHolder.stateManager.updateIsRunning(false) },
-            onInfoButtonPressed = { }, // TODO
-            areSoundEffectsEnabled = stateHolder.userPreferencesManager.areSoundEffectsEnabled.collectAsState().value,
-            onSoundEffectsToggled = stateHolder.userPreferencesManager::onAreSoundEffectsEnabledChanged,
-            isMusicEnabled = stateHolder.userPreferencesManager.isMusicEnabled.collectAsState().value,
-            onMusicToggled = stateHolder.userPreferencesManager::onIsMusicEnabledChanged,
-            isInFullscreenMode = isInFullscreenMode,
-            onFullscreenModeToggled = onFullscreenModeToggled,
+        KubrikoViewport(
+            kubriko = stateHolder.kubriko,
+            windowInsets = windowInsets,
         )
+        AnimatedVisibility(
+            visible = isGameLoaded,
+            enter = fadeIn() + scaleIn(),
+            exit = scaleOut() + fadeOut(),
+        ) {
+            SpaceSquadronMenuOverlay(
+                modifier = Modifier.windowInsetsPadding(windowInsets),
+                isGameRunning = stateHolder.stateManager.isRunning.collectAsState().value,
+                onPlayButtonPressed = { stateHolder.stateManager.updateIsRunning(true) },
+                onPauseButtonPressed = { stateHolder.stateManager.updateIsRunning(false) },
+                onInfoButtonPressed = { }, // TODO
+                areSoundEffectsEnabled = stateHolder.userPreferencesManager.areSoundEffectsEnabled.collectAsState().value,
+                onSoundEffectsToggled = stateHolder.userPreferencesManager::onAreSoundEffectsEnabledChanged,
+                isMusicEnabled = stateHolder.userPreferencesManager.isMusicEnabled.collectAsState().value,
+                onMusicToggled = stateHolder.userPreferencesManager::onIsMusicEnabledChanged,
+                isInFullscreenMode = isInFullscreenMode,
+                onFullscreenModeToggled = onFullscreenModeToggled,
+            )
+        }
     }
 }
 
@@ -92,9 +110,16 @@ private class SpaceSquadronGameStateHolderImpl : SpaceSquadronGameStateHolder {
     val stateManager = StateManager.newInstance(shouldAutoStart = false)
     private val persistenceManager = PersistenceManager.newInstance(fileName = "kubrikoSpaceSquadron")
     val userPreferencesManager = SpaceSquadronUserPreferencesManager(persistenceManager)
+    val loadingManager = SpaceSquadronLoadingManager()
+    private val musicManager = MusicManager.newInstance()
+    private val soundManager = SoundManager.newInstance()
     val spriteManager = SpriteManager.newInstance()
     val backgroundKubriko = Kubriko.newInstance(
         ShaderManager.newInstance(),
+        musicManager,
+        soundManager,
+        spriteManager,
+        loadingManager,
         SpaceSquadronBackgroundManager()
     )
     val kubriko = Kubriko.newInstance(
@@ -108,10 +133,10 @@ private class SpaceSquadronGameStateHolderImpl : SpaceSquadronGameStateHolder {
         KeyboardInputManager.newInstance(),
         PointerInputManager.newInstance(isActiveAboveViewport = true),
         SpaceSquadronGameManager(),
-        MusicManager.newInstance(),
-        SoundManager.newInstance(),
         persistenceManager,
         userPreferencesManager,
+        musicManager,
+        soundManager,
         spriteManager,
         SpaceSquadronUIManager(stateManager),
         SpaceSquadronAudioManager(stateManager, userPreferencesManager),
