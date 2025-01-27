e7e4fb5bf1780e8d106a805cc3ca3cf30e56d8a1
Pandula Péter <pandulapeter@gmail.com>
2025-01-27 14:05:18 +0200

    Replace the Galaxy background shader.

--- examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/implementation/actors/GalaxyShader.kt
+++ examples/game-space-squadron/src/commonMain/kotlin/com/pandulapeter/kubriko/gameSpaceSquadron/implementation/actors/GalaxyShader.kt
@@ -18,9 +18,8 @@ import com.pandulapeter.kubriko.shaders.Shader
 import com.pandulapeter.kubriko.shaders.extensions.ShaderUniformProvider
 
 /**
- * Credit: Kali, jurnip
- * https://www.shadertoy.com/view/XlfGRj
- * https://www.shadertoy.com/view/cdj3DW
+ * Credit: Birdmachine
+ * https://www.shadertoy.com/view/Dl2XWD
  */
 internal class GalaxyShader(
     initialState: State = State(),
@@ -61,106 +60,108 @@ internal class GalaxyShader(
 uniform float2 ${Shader.RESOLUTION};
 uniform float $TIME;
 uniform float $SPEED;
+const float colorChangeMultiplier = 30.0;
 
-const int starDensity = 16;
-const float formuParam = 0.73;
-
-const int volumeSteps = 10;
-const float stepSize = 0.12;
-const float zoom = 0.43;
-const float tile = 1.133;
-const float brightness = 0.0015;
-const float darkMatter = 0.2;
-const float distanceFade = 0.8;
-const float saturation = 0.55;
-
-const int MAX_ITER = 20;
-const vec3 NOISE_SCALE = vec3(8.3e-3, 0.3e-3, 0.11);
-const vec3 NOISE_BASE = vec3(1.3e5, 4.7e5, 2.9e5);
-
-float field(vec3 p, float s, int iter) {
-    float accum = s / 4.0;
-    float mag = 0.0;
-    float prev = 0.0;
-    float tw = 0.0;
-
-    for (int i = 0; i < MAX_ITER; ++i) {
-        if (i >= iter) {
-            break;
-        }
-        float mag = dot(p, p);
-        p = abs(p) / mag + vec3(-0.5, -0.4, -1.487);
-        float w = exp(-float(i) / 5.0);
-        accum += w * exp(-9.025 * pow(abs(mag - prev), 2.2));
-        tw += w;
-        prev = mag;
-    }
-    return max(0.0, 5.2 * accum / tw - 0.65);
+float mod289(float x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-vec4 shader2(in vec2 fragCoord) {
-    vec2 uv2 = 2.0 * fragCoord / ${Shader.RESOLUTION}.xy - 1.0;
-    vec2 uvs = uv2 * ${Shader.RESOLUTION}.xy / max(${Shader.RESOLUTION}.x, ${Shader.RESOLUTION}.y);
-    vec3 p = vec3(uvs / 2.5, 0.0) + vec3(0.8, -1.3, 0.0);
-    float freqs[4];
-    freqs[0] = 0.45;
-    freqs[1] = 0.4;
-    freqs[2] = 0.15;
-    freqs[3] = 0.9;
-    float t = field(p, freqs[2], 13);
-    vec3 p2 = vec3(uvs / 4.0, 4.0) + vec3(2.0, -1.3, -1.0);        
-    float t2 = field(p2, freqs[3], 18);
-    vec4 c2 = mix(0.5, 0.2, 1.0 - exp((abs(uv2.x) - 1.0) * 6.0)) * vec4(5.5 * t2 * t2 * t2, 2.1 * t2 * t2, 2.2 * t2 * freqs[0], t2);
-    vec4 starColor = vec4(0.0);
-    const float brightness = 1.0;
-    vec4 color = mix(freqs[3] - 0.3, 1.0, 1.0 - exp((abs(uv2.x) - 1.0) * 6.0)) * vec4(1.5 * freqs[2] * t * t * t, 1.2 * freqs[1] * t * t, freqs[3] * t, 1.0) + c2 + starColor;
-    return vec4(brightness * color.xyz, 1.0);
+vec4 mod289(vec4 x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-vec4 shader1(in vec2 fragCoord) {
-    vec2 uv = fragCoord.xy / float2(${Shader.RESOLUTION}.x, ${Shader.RESOLUTION}.x);
-    vec3 dir = vec3(uv * zoom, 10.0);
-    float time = $TIME * $SPEED + 0.25;
-    vec3 from = vec3(0.0, -time, 0.0);
+vec4 perm(vec4 x) {
+    return mod289(((x * 34.0) + 1.0) * x);
+}
 
-    // Volumetric rendering
-    float s = 0.1, fade = 1.0;
-    vec3 accumulatedColor = vec3(0.0);
+float noise(vec3 p) {
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
 
-    // Precompute constants for loops
-    float invTile = 1.0 / (tile * 2.0);
+float field1(in vec3 p,float s) {
+	float strength = 7.;
+	float accum = s/4.;
+	float prev = 0.;
+	float tw = 0.;
+	for (int i = 0; i < 26; ++i) {
+		float mag = dot(p, p);
+		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
+		float w = exp(-float(i) / 7.);
+		accum += w * exp(-strength * pow(abs(mag - prev), 2.2));
+		tw += w;
+		prev = mag;
+	}
+	return max(0., 5. * accum / tw - .7);
+}
 
-    for (int r = 0; r < volumeSteps; r++) {
-        vec3 p = from + s * dir * 0.5;
+float field2(in vec3 p, float s) {
+	float strength = 8.;
+	float accum = s/4.;
+	float prev = 0.;
+	float tw = 0.;
+	for (int i = 0; i < 18; ++i) {
+		float mag = dot(p, p);
+		p = abs(p) / mag + vec3(-.5, -.4, -1.5);
+		float w = exp(-float(i) / 7.);
+		accum += w * exp(-strength * pow(abs(mag - prev), 2.2));
+		tw += w;
+		prev = mag;
+	}
+	return max(0., 5. * accum / tw - .7);
+}
 
-        // Tiling fold optimized
-        p = abs(vec3(tile) - fract(p * invTile) * tile * 2.0);
-        float pa = 0.0, a = 0.0;
-        for (int i = 0; i < starDensity; i++) {
-            p = abs(p) / dot(p, p) - formuParam;
-            float lenP = length(p);
-            a += abs(lenP - pa);
-            pa = lenP;
-        }
-
-        float dm = max(0.0, darkMatter - a * a * 0.001); // Dark matter
-        a *= a * a; // Add contrast
-
-        fade *= (r > 6) ? 1.0 - dm : 1.0; // Avoid near dark matter
-        accumulatedColor += fade;
-        accumulatedColor += vec3(s, s * s, s * s * s * s) * a * brightness * fade; // Coloring
-
-        fade *= distanceFade; // Distance fading
-        s += stepSize;
-    }
-
-    // Color adjustment
-    accumulatedColor = mix(vec3(length(accumulatedColor)), accumulatedColor, saturation);
-    return vec4(accumulatedColor * 0.01, 1.0);
+vec3 nrand3(vec2 co) {
+	vec3 a = fract( cos( co.x*8.3e-3 + co.y )*vec3(1.3e5, 4.7e5, 2.9e5) );
+	vec3 b = fract( sin( co.x*0.3e-3 + co.y )*vec3(8.1e5, 1.0e5, 0.1e5) );
+	return mix(a, b, 0.5);
 }
 
 vec4 main(in vec2 fragCoord) {
-    return mix(shader1(fragCoord), shader2(fragCoord), 0.4);
+    float adjustedSpeed = $TIME * $SPEED;
+    vec2 uv = 2. * fragCoord.xy / ${Shader.RESOLUTION}.xy - 1.;
+	vec2 uvs = uv * ${Shader.RESOLUTION}.xy / max(${Shader.RESOLUTION}.x, ${Shader.RESOLUTION}.y);
+	vec3 p = vec3(uvs / 4., 0) + vec3(1., 1.3, 0.);
+	
+	float freqs[4];
+    float colorChange = adjustedSpeed * colorChangeMultiplier;
+	freqs[0] = noise(vec3(1, 0.25, colorChange));
+	freqs[1] = noise(vec3(7, 0.25, colorChange));
+	freqs[2] = noise(vec3(15, 0.25, colorChange));
+	freqs[3] = noise(vec3(30, 0.25, colorChange));
+
+    // Cloud layer 1
+	float t = field1(p,freqs[2]);
+	float v = (1. - exp((abs(uv.x) - 1.) * 6.)) * (1. - exp((abs(uv.y) - 1.) * 6.));
+	
+    // Cloud layer 2
+	vec3 p2 = vec3(uvs / 5.0, 1.5) + vec3(2., 1.2, -1);
+	float t2 = field2(p2,freqs[3]);
+	vec4 c2 = mix(.4, 1., v) * vec4(1.3 * t2 * t2 * t2 ,1.8  * t2 * t2 , t2* freqs[0], t2);
+	
+	// Stars layer 1
+	vec2 seed1 = floor((uvs / 4 + vec2(0.0, -adjustedSpeed * 6.48)) * ${Shader.RESOLUTION}.x);
+	vec3 rnd1 = nrand3(seed1);
+	vec4 starColor = vec4(pow(rnd1.y, 45.0));
+	
+	// Stars layer 2	
+	vec2 seed2 = floor((uvs / 4 + vec2(0.0, -adjustedSpeed * 4.18)) * ${Shader.RESOLUTION}.x);
+	vec3 rnd2 = nrand3(seed2);
+	starColor += vec4(pow(rnd2.y, 75.0));
+
+	return mix(mix(freqs[3]-.3, 1., v) * vec4(1.5*freqs[2] * t * t* t , 1.2*freqs[1] * t * t, freqs[3]*t, 1.0)+c2, starColor * 1.5, 0.65);
 }
 """
     }
