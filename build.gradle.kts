de41548ad8654ef9681208370cfad60ccc40e01a
Pandula Péter <pandulapeter@gmail.com>
2025-02-22 21:18:05 +0200

    Increment library version number to "v0.0.2".

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at
  * https://mozilla.org/MPL/2.0/.
  */
-version = "0.0.1"
+version = "0.0.2"
 "SHOWCASE_ANDROID_VERSION_NAME" set "1.0.0"
 "SHOWCASE_ANDROID_VERSION_CODE" set 1
 "SHOWCASE_ANDROID_KEY_ALIAS" set "androiddebugkey"
