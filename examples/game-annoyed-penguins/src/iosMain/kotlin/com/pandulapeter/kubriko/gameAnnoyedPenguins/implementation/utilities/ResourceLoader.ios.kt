f1e15b0bd5fadaa5a98395b60d9468bf8f1f9643
Pandula Péter <pandulapeter@gmail.com>
2025-03-01 01:17:48 +0200

    Fix web music loading in Annoyed Penguins.

---
+++ examples/game-annoyed-penguins/src/iosMain/kotlin/com/pandulapeter/kubriko/gameAnnoyedPenguins/implementation/utilities/ResourceLoader.ios.kt
@@ -0,0 +1,16 @@
+/*
+ * This file is part of Kubriko.
+ * Copyright (c) Pandula Péter 2025.
+ * https://github.com/pandulapeter/kubriko
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/.
+ */
+package com.pandulapeter.kubriko.gameAnnoyedPenguins.implementation.utilities
+
+import kubriko.examples.game_annoyed_penguins.generated.resources.Res
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+@OptIn(ExperimentalResourceApi::class)
+internal actual fun getResourceUri(path: String): String = Res.getUri(path)
