0caa5e457ec504161dbdbb84ec8f8e75dffcf45d
Pandula Péter <pandulapeter@gmail.com>
2025-03-06 10:57:52 +0200

    Improve the disclaimer box appearance.

--- app/src/webMain/kotlin/com/pandulapeter/kubrikoShowcase/implementation/ui/Disclaimer.web.kt
+++ app/src/webMain/kotlin/com/pandulapeter/kubrikoShowcase/implementation/ui/Disclaimer.web.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -39,7 +38,6 @@ import org.jetbrains.compose.resources.stringResource
 @Composable
 internal actual fun Disclaimer() = Column(
     modifier = Modifier
-        .fillMaxWidth()
         .padding(16.dp)
         .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.2f))
         .border(
