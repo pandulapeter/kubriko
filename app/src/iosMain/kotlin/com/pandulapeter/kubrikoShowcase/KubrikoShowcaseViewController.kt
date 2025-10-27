f6d97fe6e4ed4e29f3d2fe66b036387e0feba88d
Pandula Péter <pandulapeter@gmail.com>
2025-10-27 16:03:43 +0200

    Fix deprecation in iOS code.

--- app/src/iosMain/kotlin/com/pandulapeter/kubrikoShowcase/KubrikoShowcaseViewController.kt
+++ app/src/iosMain/kotlin/com/pandulapeter/kubrikoShowcase/KubrikoShowcaseViewController.kt
@@ -10,25 +10,39 @@
 package com.pandulapeter.kubrikoShowcase
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.uikit.ComposeUIViewControllerDelegate
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIApplication
+import platform.UIKit.UIViewAutoresizingFlexibleHeight
+import platform.UIKit.UIViewAutoresizingFlexibleWidth
+import platform.UIKit.UIViewController
+import platform.UIKit.addChildViewController
+import platform.UIKit.didMoveToParentViewController
 
-fun KubrikoShowcaseViewController() = ComposeUIViewController(
-    configure = {
-        delegate = object : ComposeUIViewControllerDelegate {
-            override val prefersStatusBarHidden: Boolean? get() = isInFullscreenMode.value
-        }
+@OptIn(ExperimentalForeignApi::class)
+fun KubrikoShowcaseViewController() = object : UIViewController(nibName = null, bundle = null) {
+
+    override fun prefersStatusBarHidden() = isInFullscreenMode.value
+
+    private val composeViewController = ComposeUIViewController {
+        KubrikoShowcase(
+            isInFullscreenMode = isInFullscreenMode.value,
+            getIsInFullscreenMode = { isInFullscreenMode.value },
+            onFullscreenModeToggled = {
+                isInFullscreenMode.value = !isInFullscreenMode.value
+                UIApplication.sharedApplication.keyWindow?.rootViewController?.setNeedsStatusBarAppearanceUpdate()
+            },
+        )
     }
-) {
-    KubrikoShowcase(
-        isInFullscreenMode = isInFullscreenMode.value,
-        getIsInFullscreenMode = { isInFullscreenMode.value },
-        onFullscreenModeToggled = {
-            isInFullscreenMode.value = !isInFullscreenMode.value
-            UIApplication.sharedApplication.keyWindow?.rootViewController?.setNeedsStatusBarAppearanceUpdate()
-        },
-    )
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+        addChildViewController(composeViewController)
+        view.addSubview(composeViewController.view)
+        composeViewController.didMoveToParentViewController(this)
+        composeViewController.view.setFrame(view.bounds)
+        composeViewController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth or UIViewAutoresizingFlexibleHeight
+    }
 }
 
 private val isInFullscreenMode = mutableStateOf(false)
