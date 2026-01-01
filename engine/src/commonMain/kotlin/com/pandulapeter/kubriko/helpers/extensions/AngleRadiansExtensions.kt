eab1935dcec97f65085c77ef437b258460bef4a6
jez <5053926+Ninjars@users.noreply.github.com>
2026-01-01 22:47:56 +0000

    implemented AngleRadians.rotateTowards()
    Happy to drop this commit if it's not desired. This sort of function can be very useful for performing rotations across a number of frames where the rotation should have a fixed maximum speed.

--- engine/src/commonMain/kotlin/com/pandulapeter/kubriko/helpers/extensions/AngleRadiansExtensions.kt
+++ engine/src/commonMain/kotlin/com/pandulapeter/kubriko/helpers/extensions/AngleRadiansExtensions.kt
@@ -11,6 +11,7 @@ package com.pandulapeter.kubriko.helpers.extensions
 
 import com.pandulapeter.kubriko.types.AngleRadians
 import kotlin.math.PI
+import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -19,3 +20,33 @@ val AngleRadians.deg get() = (raw * (180f / PI).toFloat()).deg
 val AngleRadians.sin get() = sin(normalized)
 
 val AngleRadians.cos get() = cos(normalized)
+
+/**
+ * Rotates this angle towards the [targetAngle] by a step not exceeding [maxDelta].
+ *
+ * If the difference between this angle and the [targetAngle] is less than [maxDelta],
+ * the returned angle will be exactly [targetAngle]. Otherwise, the angle is rotated
+ * by [maxDelta] in the direction of [targetAngle].
+ *
+ * @param targetAngle The destination angle.
+ * @param maxDelta The maximum amount to rotate by (should be non-negative).
+ * @return The new angle after rotation.
+ */
+fun AngleRadians.rotateTowards(targetAngle: AngleRadians, maxDelta: AngleRadians): AngleRadians {
+    val delta = targetAngle - this
+    return if (delta == AngleRadians.Zero || abs(delta.raw) < 0.0000001f) {
+        targetAngle
+    } else if (delta.raw < 0) {
+        if (-maxDelta > delta) {
+            this - maxDelta
+        } else {
+            this + delta
+        }
+    } else {
+        if (maxDelta < delta) {
+            this + maxDelta
+        } else {
+            this + delta
+        }
+    }
+}
