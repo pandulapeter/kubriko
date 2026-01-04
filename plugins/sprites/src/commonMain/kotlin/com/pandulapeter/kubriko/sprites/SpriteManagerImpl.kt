ee6d14b1f4dfbc3fdf9b4a79721b0f0975de1a85
jez <5053926+Ninjars@users.noreply.github.com>
2026-01-04 22:44:12 +0000

    refactored SpriteManager and ImageLoader to support rotating images in increments of i90 degrees

--- plugins/sprites/src/commonMain/kotlin/com/pandulapeter/kubriko/sprites/SpriteManagerImpl.kt
+++ plugins/sprites/src/commonMain/kotlin/com/pandulapeter/kubriko/sprites/SpriteManagerImpl.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.ImageBitmap
+import com.pandulapeter.kubriko.sprites.helpers.toSpriteResource
 import com.pandulapeter.kubriko.sprites.implementation.toImageBitmap
 import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.collections.immutable.toPersistentMap
@@ -32,10 +33,11 @@ internal class SpriteManagerImpl(
     isLoggingEnabled: Boolean,
     instanceNameForLogging: String?,
 ) : SpriteManager(isLoggingEnabled, instanceNameForLogging) {
-    private val cache = MutableStateFlow(persistentMapOf<DrawableResource, ImageBitmap?>())
+    private val cache = MutableStateFlow(persistentMapOf<SpriteResource, ImageBitmap?>())
 
     override fun getLoadingProgress(drawableResources: Collection<DrawableResource>) = if (drawableResources.isEmpty()) flowOf(1f) else cache.map { cache ->
-        cache.filter { (key, _) -> key in drawableResources }.count { (_, value) -> value != null }.toFloat() / drawableResources.size
+        val spriteResources = drawableResources.map { drawableResource -> drawableResource.toSpriteResource() }
+        cache.filter { (key, _) -> key in spriteResources }.count { (_, value) -> value != null }.toFloat() / drawableResources.size
     }
 
     override fun preload(vararg drawableResources: DrawableResource) = preload(drawableResources.toList())
@@ -45,20 +47,21 @@ internal class SpriteManagerImpl(
     }
 
     override fun get(drawableResource: DrawableResource): ImageBitmap? {
-        if (!cache.value.containsKey(drawableResource)) {
-            cache.update { it.put(drawableResource, null) }
+        val spriteResource = drawableResource.toSpriteResource()
+        if (!cache.value.containsKey(spriteResource)) {
+            cache.update { it.put(spriteResource, null) }
         }
-        return cache.value[drawableResource]
+        return cache.value[spriteResource]
     }
 
-    override fun unload(drawableResource: DrawableResource) = cache.update { it.remove(drawableResource) }
+    override fun unload(drawableResource: DrawableResource) = cache.update { it.remove(drawableResource.toSpriteResource()) }
 
     @Composable
     override fun Composable(windowInsets: WindowInsets) {
         cache.collectAsState().value // This line ensures that this Composable is invoked every time the cache is changed
         scope.launch {
             cache.update { cache ->
-                val newCache = mutableMapOf<DrawableResource, ImageBitmap?>()
+                val newCache = mutableMapOf<SpriteResource, ImageBitmap?>()
                 cache.keys.forEach { key ->
                     newCache[key] = cache[key] ?: loadImage(key)
                 }
@@ -68,8 +71,8 @@ internal class SpriteManagerImpl(
     }
 
     @OptIn(InternalResourceApi::class, ExperimentalResourceApi::class)
-    private suspend fun loadImage(drawableResource: DrawableResource): ImageBitmap? = try {
-        getDrawableResourceBytes(getSystemResourceEnvironment(), drawableResource).toImageBitmap(DensityQualifier.MDPI.dpi, DensityQualifier.MDPI.dpi)
+    private suspend fun loadImage(spriteResource: SpriteResource): ImageBitmap? = try {
+        getDrawableResourceBytes(getSystemResourceEnvironment(), spriteResource.drawableResource).toImageBitmap(DensityQualifier.MDPI.dpi, DensityQualifier.MDPI.dpi, spriteResource.rotation)
     } catch (e: Exception) {
         e.printStackTrace()
         null
